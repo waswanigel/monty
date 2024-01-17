e0abc84492a1454359e8769a778b906a347e0961
EmmanuelNyabuto <emmanuele2ke@gmail.com>
2024-01-17 02:03:54 -0800

    prints string starting at top of stack

---
+++ pstr.c
@@ -0,0 +1,25 @@
+#include "monty.h"
+/**
+ * f_pstr - prints string starting at top of stack,
+ * followed by a new
+ * @head: stack head
+ * @counter: line_number
+ * Return: no return
+*/
+void f_pstr(stack_t **head, unsigned int counter)
+{
+	stack_t *h;
+	(void)counter;
+
+	h = *head;
+	while (h)
+	{
+		if (h->n > 127 || h->n <= 0)
+		{
+			break;
+		}
+		printf("%c", h->n);
+		h = h->next;
+	}
+	printf("\n");
+}
