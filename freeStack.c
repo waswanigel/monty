853ef0ecb946bf363c56a4f6ccbf8ad10033cad4
EmmanuelNyabuto <emmanuele2ke@gmail.com>
2024-01-18 05:50:03 -0800

    freeStack.c

--- freeStack.c
+++ freeStack.c
@@ -1,7 +1,7 @@
 #include "monty.h"
 
 /**
- * free_stack - frees a doubly linked list
+ * free_stack - will free a doubly linked list
  * @head: head of the stack
  */
 void free_stack(stack_t *head)
